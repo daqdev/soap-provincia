7e2f622fffbcfcace384411eaa534efbfcb95370
decudev <daquiro@pm.me>
2023-01-07 03:20:51 -0300

    first commit

---
+++ src/test/java/com/decu/soapprovincia/SoapProvinciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decu.soapprovincia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapProvinciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
